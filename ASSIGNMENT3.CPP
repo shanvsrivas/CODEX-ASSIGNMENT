f2252a3b57ecf52ecac2d6c876e439b6ab5a1128
shanvsrivas <146552717+shanvsrivas@users.noreply.github.com>
2023-11-21 19:35:05 +0530

    CODEX ASSIGNMENT3.CPP

---
+++ ASSIGNMENT3.CPP
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+inline int addNumbers(int a, int b)
+{
+    return a + b;
+}
+
+int multiplyNumbers(int x, int y, int z = 1) 
+{
+    return x * y * z;
+}
+
+int main() 
+{
+    int sum = addNumbers(5, 7);
+    int product = multiplyNumbers(3, 4); // Use default argument
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    return 0;
+}
